e1551fa24ff23b227f3146c4a4f50bca6f66dfb2
Sophia Ngo <sophia.ngo@sjsu.edu>
2015-11-06 14:58:23 -0800

    Fixed endpoints to handle more than 1 available thermostat and added endpoint to change temperature by name

--- routes/thermo.js
+++ routes/thermo.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var nest = require('unofficial-nest-api');
 // var username = 'sophia2901@gmail.com';
-// var password = 'test@CMPE295';
+// var password = 'Cmpe@295';
 
 /*
 POST /thermo/addNew
@@ -20,7 +20,7 @@ router.post('/new', function addNewThermo(req,res){
   nest.login(username, password, function (err, data) {
     if (err) {
         res.status(400);
-        res.send('Invalid login credentials');
+        res.send(err.message);
     } else {
       nest.fetchStatus(function (data) {
         if (!data){
@@ -28,18 +28,30 @@ router.post('/new', function addNewThermo(req,res){
           res.send('No data response');
         } else {
           var response = '';
+          var device;
           for (var deviceId in data.device) {
+            //var object = JSON.stringify(data.device);
+            //console.log(object);
               if (data.device.hasOwnProperty(deviceId)) {
-                  var device = data.shared[deviceId];
-                  var name = device.name;
-                  response = response + name;
+                  device = data.shared[deviceId];
+                  //console.log(device);
+                  response = response + (JSON.stringify(device));
+                  // var name = device.name;
+                  // var current_temp = device.current_temperature;
+                  // var target_temp = device.target_temperature;
+                  // var humidity = device.humidity;
+                  //response = response + name + ' , ' + current_temp + ' , ' + target_temp + ' , ' + humidity;
                   //console.log('name: ' + name);
               }
+                response = response + '/';
           }
           //subscribe();
-          console.log('response: ' +  response);
+          //console.log('response: ' +  response);
+          //var returned_object = JSON.stringify(response);
+          //console.log('returned_object: ' +  returned_object);
           res.status(200);
-          res.JSON(response);
+          //res.send(returned_object.toString());
+          res.send(response.toString());
         }
       }); //  fetchStatus callback
     } // else
@@ -63,20 +75,59 @@ router.post('/new', function addNewThermo(req,res){
 //     setTimeout(subscribe, 2000);
 // };
 
-router.post('/temp/:temp', function changeTemp(req,res){
-  var updated_temp = req.params.temp;
-  nest.fetchStatus(function (data) {
-    for (var deviceId in data.device) {
-            if (data.device.hasOwnProperty(deviceId)) {
-                var device = data.shared[deviceId];
-                // here's the device and ID
+router.post('/:thermo_name/temp/:updated_temp', function changeTempByName(req,res){
+    var thermo_name = req.params.thermo_name;
+    var updated_temp = req.params.updated_temp;
+    var success = false;
+    nest.fetchStatus(function (data) {
+      for (var deviceId in data.device) {
+        if (data.device.hasOwnProperty(deviceId)) {
+            var device = data.shared[deviceId];
+            if (device.name == thermo_name){
                 nest.setTemperature(deviceId, updated_temp);
+                if(device.current_temperature==updated_temp){
+                  success = true;
+                }
             }
-    }
-    res.send("Successfully change temperature to " + updated_temp);
+        }
+      }
+      if (success == true){
+        res.status(200);
+        res.send("Successfully change temperature to " + updated_temp);
+      } else {
+        res.status(400);
+        res.send("Unable to change the temperature. Please try again later");
+      }
     });
 });
 
+router.post('/all/temp/:temp', function changeTemp(req,res){
+  var updated_temp = req.params.temp;
+  var successCount = 0;
+  var deviceCount = 0;
+  nest.fetchStatus(function (data) {
+    for (var deviceId in data.device) {
+      if (data.device.hasOwnProperty(deviceId)) {
+         deviceCount++;
+         var device = data.shared[deviceId];
+                // here's the device and ID
+         nest.setTemperature(deviceId, updated_temp);
+         if(device.current_temperature==updated_temp){
+            successCount++;
+        }
+      }
+    }
+    if (successCount == deviceCount){
+      res.status(200);
+      res.send("Successfully change temperature for " + successCount + " thermostats");
+    } else {
+      res.status(400);
+      res.send("Unable to change the temperature. Please try again later");
+    }
+  });
+});
+
+// When setting away mode, all thermostat in the houses will be change status
 router.post('/away', function setAwayMode(req,res){
   nest.fetchStatus(function (data) {
    if (!data){
@@ -95,6 +146,7 @@ router.post('/away', function setAwayMode(req,res){
   });
 });
 
+// When setting away mode, all thermostat in the houses will be change status
 router.post('/home', function setAwayMode(req,res){
   nest.fetchStatus(function (data) {
    if (!data){
