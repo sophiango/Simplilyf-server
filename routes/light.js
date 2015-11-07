28d5ab8a661a898f57ab61f8d51cf2709a5f02c1
jainprachi25 <jainprachi.55@gmail.com>
2015-11-06 19:38:08 -0800

    updated light code

--- routes/light.js
+++ routes/light.js
@@ -5,10 +5,10 @@ var Hue = require('philips-hue-api');
 var username = 'newdeveloper';
 var Light = require('../models/light');
 
-app.use(logger('dev'));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+//app.use(logger('dev'));
+//app.use(cookieParser());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: true}));
 
 var username = 'newdeveloper'
 var username = '1ed3f31c3acf5785314ef90f6354675';
@@ -41,6 +41,13 @@ router.get('/getlight/:light_id', function(req,res){
   var light_id = req.params.light_id;
   hue = Hue(LOCAL_URL);
   hue.lights(parseInt(light_id)).getState(function(error, light){
+    if(error){
+      res.status(400);
+      res.send('Error: ' + error);
+    } else {
+      res.status(200);
+      res.send(light);
+    }
     console.log(light);
 
     var date = new Date();
@@ -56,13 +63,6 @@ router.get('/getlight/:light_id', function(req,res){
     newLight.reachable = light.state.reachable;
     newLight.type = light.type;
     newLight.save();
-    if(error){
-      res.status(400);
-      res.send('Error: ' + error);
-    } else {
-      res.status(200);
-      res.send(light);
-    }
   });
 });
 
@@ -72,16 +72,22 @@ router.post('/on/:light_id', function(req,res){
   //console.log("light id: " + light_id);
   hue = Hue(LOCAL_URL);
   hue.lights(parseInt(light_id)).on();
-  hue.lights(parseInt(light_id)).getState(function(error, res){
-    console.log(res.state.on);
+  hue.lights(parseInt(light_id)).getState(function(error, light){
+    if(error){
+        res.status(400);
+        res.send('Error: ' + error);
+      } else {
+        res.status(200);
+        res.send(light);
+      }
+    console.log(light.state.on);
     console.log("light:" + light_id + " switched on!!");
-
-    var getLight = Light.findOne({'id' :  light_id}).sort('currentTS':-1).limit(1).exec(
-      function(error, light){
+    var getLight = Light.findOne({'id' :  light_id}).sort({'currentTS' : -1}).limit(1).exec(
+      function(error, res){
         if(error){
           console.log(error);
         }
-        else if(!light){
+        else if(!res){
           console.log("Light with id:" + light_id + "is not found");
         }
         else{
@@ -90,68 +96,28 @@ router.post('/on/:light_id', function(req,res){
           var date = new Date();
           var newLight = new Light();
           newLight.id = light_id;
-          newLight.name = light.name;
+          newLight.name = res.name;
           newLight.currentTS = date;
-          newLight.modelid = light.modelid;
-          newLight.onStatus = res.state.on;
-          newLight.bri = light.bri;
-          newLight.hue = light.hue;
-          newLight.sat = light.sat;
-          newLight.reachable = light.reachable;
-          newLight.type = light.type;
+          newLight.modelid = res.modelid;
+          newLight.onStatus = light.state.on;
+          newLight.bri = res.bri;
+          newLight.hue = res.hue;
+          newLight.sat = res.sat;
+          newLight.reachable = res.reachable;
+          newLight.type = res.type;
           newLight.save();
         }
       }
     );
-  hue.lights(parseInt(light_id)).getState(function(error, light){
-    if(error){
-      res.status(400);
-      res.send('Error: ' + error);
-    } else {
-      res.status(200);
-      res.send(light);
-    }
-    //console.log(res.state.on);
-    //console.log("light:" + light_id + " switched on!!");
   });
 });
 
 // switching light off
 router.post('/off/:light_id', function(req,res){
   var light_id = req.params.light_id;
-  console.log("light id: " + light_id);
+  //console.log("light id: " + light_id);
   hue = Hue('http://localhost:8000/api/newdeveloper/');
   hue.lights(parseInt(light_id)).off();
-  hue.lights(parseInt(light_id)).getState(function(error, res){
-    console.log(res.state.on);
-    console.log("light:" + light_id + " switched off!!");
-    var getLight = Light.findOne({'id' :  light_id}).sort('currentTS':-1).limit(1).exec(
-      function(error, light){
-        if(error){
-          console.log(error);
-        }
-        else if(!light){
-          console.log("Light with id:" + light_id + "is not found");
-        }
-        else{
-          console.log("Light with id:" + light_id + "is found");
-
-          var date = new Date();
-          var newLight = new Light();
-          newLight.id = light_id;
-          newLight.name = light.name;
-          newLight.currentTS = date;
-          newLight.modelid = light.modelid;
-          newLight.onStatus = res.state.on;
-          newLight.bri = light.bri;
-          newLight.hue = light.hue;
-          newLight.sat = light.sat;
-          newLight.reachable = light.reachable;
-          newLight.type = light.type;
-          newLight.save();
-        }
-      }
-    );
   hue.lights(parseInt(light_id)).getState(function(error, light){
     if(error){
       res.status(400);
@@ -160,8 +126,35 @@ router.post('/off/:light_id', function(req,res){
       res.status(200);
       res.send(light);
     }
-    // console.log(res.state.on);
-    // console.log("light:" + light_id + " switched off!!");
+    console.log(light.state.on);
+    console.log("light:" + light_id + " switched off!!");
+    var getLight = Light.findOne({'id' :  light_id}).sort({'currentTS' : -1}).limit(1).exec(
+      function(error, res){
+        if(error){
+          console.log(error);
+        }
+        else if(!res){
+          console.log("Light with id:" + light_id + "is not found");
+        }
+        else{
+          console.log("Light with id:" + light_id + "is found");
+
+          var date = new Date();
+          var newLight = new Light();
+          newLight.id = light_id;
+          newLight.name = res.name;
+          newLight.currentTS = date;
+          newLight.modelid = res.modelid;
+          newLight.onStatus = light.state.on;
+          newLight.bri = res.bri;
+          newLight.hue = res.hue;
+          newLight.sat = res.sat;
+          newLight.reachable = res.reachable;
+          newLight.type = res.type;
+          newLight.save();
+        }
+      }
+    );
   });
 });
 
@@ -172,16 +165,23 @@ router.post('/change/:light_id/:colorname', function(req,res){
   var new_color = req.params.colorname;
     hue = Hue('http://localhost:8000/api/newdeveloper/');
     hue.lights(parseInt(light_id)).lightcolor(new_color);
-    hue.lights(parseInt(light_id)).getState(function(error, res){
-      console.log(res.state.hue + "==>" + new_color);
+    hue.lights(parseInt(light_id)).getState(function(error, light){
+      if(error){
+      res.status(400);
+      res.send('Error: ' + error);
+    } else {
+      res.status(200);
+      res.send(light);
+    }
+      console.log(light.state.hue + "==>" + new_color);
       console.log("Color is changed to " + new_color + "for light: " + light_id);
 
-      var getLight = Light.findOne({'id' :  light_id}).sort('currentTS':-1).limit(1).exec(
-      function(error, light){
+      var getLight = Light.findOne({'id' :  light_id}).sort({'currentTS' : -1}).limit(1).exec(
+      function(error, res){
         if(error){
           console.log(error);
         }
-        else if(!light){
+        else if(!res){
           console.log("Light with id:" + light_id + "is not found");
         }
         else{
@@ -190,34 +190,20 @@ router.post('/change/:light_id/:colorname', function(req,res){
           var date = new Date();
           var newLight = new Light();
           newLight.id = light_id;
-          newLight.name = light.name;
+          newLight.name = res.name;
           newLight.currentTS = date;
-          newLight.modelid = light.modelid;
-          newLight.onStatus = light.onStatus;
-          newLight.bri = light.bri;
-          newLight.hue = res.state.hue;
-          newLight.sat = light.sat;
-          newLight.reachable = light.reachable;
-          newLight.type = light.type;
+          newLight.modelid = res.modelid;
+          newLight.onStatus = res.onStatus;
+          newLight.bri = res.bri;
+          newLight.hue = light.state.hue;
+          newLight.sat = res.sat;
+          newLight.reachable = res.reachable;
+          newLight.type = res.type;
           newLight.save();
         }
       }
     );
     });
   });  
-  hue = Hue(LOCAL_URL);
-  hue.lights(parseInt(light_id)).lightcolor(new_color);
-  hue.lights(parseInt(light_id)).getState(function(error, light){
-    if(error){
-      res.status(400);
-      res.send('Error: ' + error);
-    } else {
-      res.status(200);
-      res.send(light);
-    }
-    // console.log(res.state.hue);
-    // console.log("Color is changed to " + new_color + "for light: " + light_id);
-  });
-});
 
 module.exports = router;
