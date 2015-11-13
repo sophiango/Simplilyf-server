7005e2e44886ecc1fcb8e8b13648a32a32e1c54b
Sophia Ngo <sophia.ngo@sjsu.edu>
2015-11-13 11:38:56 -0800

    integrate voice into node server codebase

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 module.exports = {
-	'url' : 'mongodb://cmpe:295@ds027483.mongolab.com:27483/cmpe295'
+	// 'url' : 'mongodb://cmpe:295@ds027483.mongolab.com:27483/cmpe295'
+	'url' : 'mongodb://cmpe:295@ds041571.mongolab.com:41571/team295'
 }
---
+++ models/voicemodel.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+// mongoose.connect('mongodb://cmpe:295@ds041571.mongolab.com:41571/team295',function(err){
+// 	if(err){
+// 		console.log("error to connect to mongodb");
+// 	}else{
+// 		console.log("connection to mongodb successfull");
+// 	}
+// });
+//create instance of schema
+var mongoSchema = new mongoose.Schema({
+"serial_no": String,
+"increase_temp":{
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "minItems": 1
+
+        },
+
+"decrease_temp":{
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "minItems": 1
+        },
+"on_lights":{
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "minItems": 1
+
+        },
+
+"off_lights":{
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "minItems": 1
+        },
+"turn_on":{
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "minItems": 1
+        },
+"turn_off":{
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "minItems": 1
+        },
+
+});
+//create a schema
+
+
+//create a model/collection if it does not exist
+module.exports=mongoose.model('smartdevices',mongoSchema);
