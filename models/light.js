913dea90f8b209e643178786827e096212d55cba
jainprachi25 <jainprachi.55@gmail.com>
2015-11-13 17:14:05 -0800

    updated model

--- models/light.js
+++ models/light.js
@@ -3,14 +3,15 @@ var mongoose = require ('mongoose');
 var LightSchema = new mongoose.Schema({
 	id : String,
 	name : String,
-  	currentTS : Date,
+  	currentTS : {type:Date, default: Date.now},
 	modelid : String,
 	onStatus : Boolean,
 	bri : Number,
   	hue : Number,
 	sat : Number,
 	reachable : Boolean,
-  	type : String
+  	type : String,
+  	operation : Number
 });
 
 module.exports = mongoose.model('Light', LightSchema, 'light_data');
