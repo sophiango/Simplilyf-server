2b9dfcf1827095f4e810d43f7d7c6f73ef954c6e
Sophia Ngo <sophia.ngo@sjsu.edu>
2015-11-13 17:22:19 -0800

    change thermo model to persist data

--- models/thermo.js
+++ models/thermo.js
@@ -1,29 +1,41 @@
 var mongoose = require ('mongoose');
 
 var ThermoSchema = mongoose.Schema({
-  device_id : String,
-	name : String,
-  where_id : String,
-	target_temperature_f : Number,
-	target_temperature_c : Number,
-	target_temperature_high_f : Number,
-  target_temperature_high_c : Number,
-	target_temperature_low_f : Number,
-	target_temperature_low_c : Number,
-  away_temperature_high_f : Number,
-	away_temperature_high_c : Number,
-	away_temperature_low_f : Number,
-	away_temperature_low_c : Number,
-  ambient_temperature_f : Number,
-	ambient_temperature_c : Number,
-  humidity : Number,
-  can_cool : Boolean,
-	can_heat : Boolean,
-	has_leaf : Boolean,
-	hvac_mode : String,
-	is_using_emergency_heat : Boolean,
-	has_fan : Boolean,
-  timestamp : Date
+  created_at:{type:Date, default: Date.now},
+  thermo_name : String,
+  current_temp : Number,
+  user_id : String,
+  thermo_id : String,
+  thermo_mode : String,
+  operation : Number
 });
+
+// var ThermoSchema = mongoose.Schema({
+//     $version : Number,
+//     $timestamp : Number,
+//     auto_away : Number,
+//     auto_away_learning : String,
+//     can_cool : Boolean,
+//     can_heat : Boolean,
+//     compressor_lockout_enabled : Boolean,
+//     compressor_lockout_timeout : Number,
+//     current_temperature : Number,
+//     hvac_ac_state : Boolean,
+//     hvac_alt_heat_state : Boolean,
+//     hvac_alt_heat_x2_state : Boolean,
+//     hvac_aux_heater_state : Boolean,
+//     hvac_cool_x2_state : Boolean,
+//     hvac_emer_heat_state : Boolean,
+//     hvac_fan_state : Boolean,
+//     hvac_heat_x2_state : Boolean,
+//     hvac_heat_x3_state : Boolean,
+//     hvac_heater_state : Boolean,
+//     name : String,
+//     target_change_pending : Boolean,
+//     target_temperature : Number,
+//     target_temperature_high : Number,
+//     target_temperature_low : Number,
+//     target_temperature_type : String
+// });
 
 module.exports = mongoose.model('Thermo', ThermoSchema);
